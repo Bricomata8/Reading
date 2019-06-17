7ce95d0a20eda8632fbaaf77d8a807b7d79a4c16
aghiles <githubfortyuds@gmail.com>
2019-06-17 22:16:16 +0200

    commit

---
+++ setup/lib/iot/storage/3Q425WTB/satellite-5cf7bd7564746d596c000241.js
@@ -0,0 +1,10 @@
+gtagjs = document.createElement('script');
+gtagjs.setAttribute('src','https://www.googletagmanager.com/gtag/js?id=AW-973395369')
+gtagjs.setAttribute('async','true')
+document.getElementsByTagName('head')[0].appendChild(gtagjs);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'AW-973395369');
+
