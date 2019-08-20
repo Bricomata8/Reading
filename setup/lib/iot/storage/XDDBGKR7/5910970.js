ca8c0e8ae87d2cf8cba67982ab307b2b077d46ed
aghiles <githubfortyuds@gmail.com>
2019-08-20 12:47:43 +0200

    commit

--- setup/lib/iot/storage/XDDBGKR7/5910970.js
+++
@@ -1,4 +0,0 @@
-(function(id,src){if(document.getElementById(id)){return;}
-var js=document.createElement('script');js.src=src;js.type='text/javascript';js.id=id;var e=document.getElementsByTagName('script')[0];e.parentNode.insertBefore(js,e);})('hs-analytics','//js.hs-analytics.net/analytics/1565206200000/5910970.js');(function(id,src,attrs){if(document.getElementById(id)){return;}
-var js=document.createElement('script');js.src=src;js.type='text/javascript';js.id=id;for(var name in attrs){if(attrs.hasOwnProperty(name)){js.setAttribute(name,attrs[name]);}}
-var e=document.getElementsByTagName('script')[0];e.parentNode.insertBefore(js,e);})('hs-ads-pixel-5910970','https://js.hsadspixel.net/fb.js',{"data-ads-portal-id":5910970,"data-ads-env":"prod","data-loader":"hs-scriptloader","data-hsjs-portal":5910970,"data-hsjs-env":"prod"});
